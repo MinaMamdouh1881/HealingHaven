65794fcfbc9e53781f481a89d615ec135a170537
Mina Mamdouh <minamamdouh345@gmail.com>
2025-06-15 19:28:23 +0300

    fix some ui and testing resender

--- app/Actions/appointmentAction.ts
+++ app/Actions/appointmentAction.ts
@@ -1,4 +1,21 @@
-"use server";
+'use server';
+import { Resend } from 'resend';
 export default async function appointmentAction(formData: FormData) {
-  console.log(formData);
+  const apiKey = process.env.RESEND_API_KEY;
+  if (!apiKey) {
+    throw new Error('Missing RESEND_API_KEY environment variable.');
+  }
+  const resend = new Resend(apiKey);
+  try {
+    const res = await resend.emails.send({
+      from: formData.get('email') as string,
+      to: 'menamamdouh852456@gmail.com',
+      subject: 'Appointment Request',
+      text: 'You have received a new appointment request.',
+    });
+    console.log(res);
+  } catch (error) {
+    console.log('Error sending email:', error);
+  }
+  // console.log(formData);
 }
--- app/components/OurServices.tsx
+++ app/components/OurServices.tsx
@@ -1,62 +1,65 @@
-import { CiWavePulse1 } from "react-icons/ci";
-import { FaHeartbeat } from "react-icons/fa";
-import { GiStomach } from "react-icons/gi";
-import { GiBrain } from "react-icons/gi";
-import { BsLungsFill } from "react-icons/bs";
+import { CiWavePulse1 } from 'react-icons/ci';
+import { FaHeartbeat } from 'react-icons/fa';
+import { GiStomach } from 'react-icons/gi';
+import { GiBrain } from 'react-icons/gi';
+import { BsLungsFill } from 'react-icons/bs';
 
 function OurServices() {
   const services = [
     {
-      icon: <FaHeartbeat size={40} className="text-secbg" />,
-      title: "Cardiology",
+      icon: <FaHeartbeat size={40} className='text-secbg' />,
+      title: 'Cardiology',
       description:
-        "It uses a dictionary over combined with a handful of model sentence.",
-      link: "Explore Now",
+        'It uses a dictionary over combined with a handful of model sentence.',
+      link: 'Explore Now',
     },
     {
-      icon: <GiStomach size={40} className="text-secbg" />,
-      title: "Gastroenterology",
+      icon: <GiStomach size={40} className='text-secbg' />,
+      title: 'Gastroenterology',
       description:
-        "It uses a dictionary over combined with a handful of model sentence.",
-      link: "Explore Now",
+        'It uses a dictionary over combined with a handful of model sentence.',
+      link: 'Explore Now',
     },
     {
-      icon: <GiBrain size={40} className="text-secbg" />,
-      title: "Neurology",
+      icon: <GiBrain size={40} className='text-secbg' />,
+      title: 'Neurology',
       description:
-        "It uses a dictionary over combined with a handful of model sentence.",
-      link: "Explore Now",
+        'It uses a dictionary over combined with a handful of model sentence.',
+      link: 'Explore Now',
     },
     {
-      icon: <BsLungsFill size={40} className="text-secbg" />,
-      title: "Pulmonology",
+      icon: <BsLungsFill size={40} className='text-secbg' />,
+      title: 'Pulmonology',
       description:
-        "It uses a dictionary over combined with a handful of model sentence.",
-      link: "Explore Now",
+        'It uses a dictionary over combined with a handful of model sentence.',
+      link: 'Explore Now',
     },
   ];
   return (
-    <div className="mt-[20px] md:mt-main bg-secbg px-4 md:px-main py-main">
-      <div className="flex flex-row gap-3 md:gap-5 items-center text-white font-bold text-center justify-center">
-        <CiWavePulse1 size={30} className="font-extrabold text-white" />
+    <div className='mt-[20px] md:mt-main bg-secbg px-4 md:px-main py-main'>
+      <div className='flex flex-row gap-3 md:gap-5 items-center text-white font-bold text-center justify-center'>
+        <CiWavePulse1 size={30} className='font-extrabold text-white' />
         <p>Our Services</p>
       </div>
-      <h2 className="text-2xl lg:text-5xl font-semibold leading-15 md:leading-15 text-center text-white">
+      <h2 className='text-2xl lg:text-5xl font-semibold leading-15 md:leading-15 text-center text-white'>
         Experienced in a variety of
         <br /> medical services
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-18 gap-5">
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-18 gap-5'>
         {services.map((el, i) => (
           <div
             key={i}
-            className="flex flex-col bg-mainbg rounded-lg p-5 gap-10"
+            className='flex flex-col bg-mainbg rounded-lg p-5 gap-10 hover:bg-gray-300 duration-500'
           >
-            <span className="self-center shadow-[0px_0px_30px_0px_#999] rounded-full p-2">
+            <span className='self-center shadow-[0px_0px_30px_0px_#999] rounded-full p-2'>
               {el.icon}
             </span>
-            <h3 className="self-center text-2xl font-semibold">{el.title}</h3>
-            <p className="text-lg text-gray-600">{el.description}</p>
-            <a href="#" className="flex flex-row gap-2 items-center p-2 w-fit">
+            <h3 className='self-center text-2xl font-semibold'>{el.title}</h3>
+            <p className='text-lg text-gray-600'>{el.description}</p>
+            <a
+              href='#'
+              className='flex flex-row gap-2 items-center p-2 w-fit text-secbg font-semibold'
+            >
               {el.link}
             </a>
           </div>
